a5cd389accbb5223da4839c3c48bab96f92ca0f6
OHEUNSOL <eunsol037@naver.com>
2025-06-23 02:33:25 +0900

    feat : dto 추가

--- src/main/java/com/goorm/tablepick/payment/dto/PaymentRequestDto.java
+++ src/main/java/com/goorm/tablepick/payment/dto/PaymentRequestDto.java
@@ -1,12 +1,18 @@
 package com.goorm.tablepick.payment.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentRequestDto {
     private Long reservationId;
-    private Long userId;
-    private int amount;
+    private Long memberId;
+    private Long amount;
 }
