274e32c5b53125db29c6409876f5fc690219c781
OHEUNSOL <eunsol037@naver.com>
2025-06-25 14:56:40 +0900

    feat : kafka 설정 추가 및 변경

--- src/main/java/com/goorm/tablepick/payment/event/model/PaymentRequestEvent.java
+++ src/main/java/com/goorm/tablepick/payment/event/model/PaymentRequestEvent.java
@@ -3,8 +3,10 @@ package com.goorm.tablepick.payment.event.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class PaymentRequestEvent {
